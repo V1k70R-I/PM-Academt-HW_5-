df56143fe0716be36fe6c48b09c23b7c433bd540
viktor.shslyapin <v.shalyapin3323@gmail.com>
2021-01-04 16:06:03 +0200

    fixed Task_3.4

--- HomeWork_5/Task_3_String/Task_3.4/main.js
+++ HomeWork_5/Task_3_String/Task_3.4/main.js
@@ -1,13 +1,13 @@
-let fullPage = document.documentElement.outerHTML; 
+const fullPage = document.documentElement.outerHTML; 
 
-let regular4HexColors = /#[0-9|a-f]{3,6}/g;
-let foundHexColors = fullPage.match(regular4HexColors);
+const regular4HexColors = /#[0-9|a-f]{3,6}/g;
+const foundHexColors = fullPage.match(regular4HexColors);
 
-let regular4PhoneNumber = /\+ \(380\)[-| ]0(67|68|96|97|98|50|66|95|99|63|73|93) [0-9]{3} [0-9]{2} [0-9]{2}/g;
-let foundPhoneNumbers = fullPage.match(regular4PhoneNumber);
+const regular4PhoneNumber = /\+ \(380\)[-| ]0(67|68|96|97|98|50|66|95|99|63|73|93) [0-9]{3} [0-9]{2} [0-9]{2}/g;
+const foundPhoneNumbers = fullPage.match(regular4PhoneNumber);
 
-let regular4Script = /<script[^>]+>|<\/script>/g;
-let foundScript = fullPage.match(regular4Script);
+const regular4Script = /<script[^>]+>|<\/script>/g;
+const foundScript = fullPage.match(regular4Script);
 
 console.log(`Все значения цветов #HEX в  HTML - ${foundHexColors}`);
 console.log(`Все вхождения номера телефона в стандартном виде - ${foundPhoneNumbers}`);
