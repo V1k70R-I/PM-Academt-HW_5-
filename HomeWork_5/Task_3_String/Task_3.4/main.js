1d2e373bd47fdbe5cf7bd69813e28d215dd3556f
viktor.shslyapin <v.shalyapin3323@gmail.com>
2021-01-10 21:27:26 +0200

    updated task 3.4

--- HomeWork_5/Task_3_String/Task_3.4/main.js
+++ HomeWork_5/Task_3_String/Task_3.4/main.js
@@ -1,9 +1,9 @@
 const fullPage = document.documentElement.outerHTML; 
 
-const regular4HexColors = /#[0-9|a-f]{3,6}/g;
+const regular4HexColors = /#[\d|a-f]{3,6}/g;
 const foundHexColors = fullPage.match(regular4HexColors);
 
-const regular4PhoneNumber = /\+ \(380\)[-| ]0(67|68|96|97|98|50|66|95|99|63|73|93) [0-9]{3} [0-9]{2} [0-9]{2}/g;
+const regular4PhoneNumber = /\+ \(380\)[-| ]0(67|68|96|97|98|50|66|95|99|63|73|93) \d{3} \d{2} \d{2}/g;
 const foundPhoneNumbers = fullPage.match(regular4PhoneNumber);
 
 const regular4Script = /<script[^>]+>|<\/script>/g;
