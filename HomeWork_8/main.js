26bf45151dbeed5296493b4e2ec5b55fe0fe52bb
viktor.shslyapin <v.shalyapin3323@gmail.com>
2021-02-26 11:36:23 +0200

    little fix

--- HomeWork_8/main.js
+++ HomeWork_8/main.js
@@ -1,15 +1,21 @@
 // Task 1
 
-const assert = require('assert');
+const assert = require("assert");
 
 function multiplyBy(multiplicator, ...nums) {
-    return nums.map((num) => num * multiplicator);
+  return nums.map((num) => num * multiplicator);
 }
 
 assert.deepStrictEqual(multiplyBy(2, 3, 4, 5), [6, 8, 10]);
-assert.deepStrictEqual(multiplyBy(10, 20, 40, 100, 200, 3), [200, 400, 1000, 2000, 30]);
+assert.deepStrictEqual(multiplyBy(10, 20, 40, 100, 200, 3), [
+  200,
+  400,
+  1000,
+  2000,
+  30,
+]);
 
-console.log('Looks good');
+console.log("Looks good");
 
 // Task 2
 
@@ -17,36 +23,51 @@ function flatWhite(arr) {
   return [...arr.flat()];
 }
 
-assert.deepStrictEqual(flatWhite(['doppio', ['hot'], 'milk']), ['doppio', 'hot', 'milk']);
-assert.deepStrictEqual(flatWhite([['espresso', 'hot'], 'milk']), ['espresso', 'hot', 'milk']);
+assert.deepStrictEqual(flatWhite(["doppio", ["hot"], "milk"]), [
+  "doppio",
+  "hot",
+  "milk",
+]);
+assert.deepStrictEqual(flatWhite([["espresso", "hot"], "milk"]), [
+  "espresso",
+  "hot",
+  "milk",
+]);
 
-console.log('Looks good');
+console.log("Looks good");
 
 // Task 3
 
 function extendWith(...[first, second]) {
-    for (const prop in second) {
-        if (!first.hasOwnProperty(prop)) {
-            first[prop] = second[prop];
-        }
+  for (const prop in second) {
+    if (!first.hasOwnProperty(prop)) {
+      first[prop] = second[prop];
     }
+  }
 
-    return first;
+  return first;
 }
 
 assert.deepStrictEqual(
-    extendWith(
-        { flatWhite: ['doppio', 'hot', 'milk'], isValid: true },
-        { isValid: false, additionalProp: { thisIsGoodProp: 123 } }
-    ),
-    { flatWhite: ['doppio', 'hot', 'milk'], isValid: true, additionalProp: { thisIsGoodProp: 123 } }
+  extendWith(
+    { flatWhite: ["doppio", "hot", "milk"], isValid: true },
+    { isValid: false, additionalProp: { thisIsGoodProp: 123 } }
+  ),
+  {
+    flatWhite: ["doppio", "hot", "milk"],
+    isValid: true,
+    additionalProp: { thisIsGoodProp: 123 },
+  }
 );
 assert.deepStrictEqual(
-    extendWith({ isValid: false, myMax: 9990 }, { isValidos: false, myMin: 8999 }),
-    { isValid: false, myMax: 9990, isValidos: false, myMin: 8999 }
+  extendWith(
+    { isValid: false, myMax: 9990 },
+    { isValidos: false, myMin: 8999 }
+  ),
+  { isValid: false, myMax: 9990, isValidos: false, myMin: 8999 }
 );
 
-console.log('Looks good');
+console.log("Looks good");
 
 // Task 4
 
@@ -54,11 +75,11 @@ function extendWithEndless(...args) {
   const result = {};
 
   for (const obj of args) {
-      for (const prop in obj) {
-          if (obj.hasOwnProperty(prop)) {
-              result[prop] = obj[prop];
-          }
+    for (const prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+        result[prop] = obj[prop];
       }
+    }
   }
 
   return result;
@@ -66,19 +87,19 @@ function extendWithEndless(...args) {
 
 assert.deepStrictEqual(
   extendWithEndless(
-      { flatWhite: ['doppio', 'hot', 'milk'], isValid: true },
-      { isValid: false, additionalProp: { thisIsGoodProp: 123 } },
-      { prop3: true },
-      { prop4: true },
-      { isValid: [false, false] }
+    { flatWhite: ["doppio", "hot", "milk"], isValid: true },
+    { isValid: false, additionalProp: { thisIsGoodProp: 123 } },
+    { prop3: true },
+    { prop4: true },
+    { isValid: [false, false] }
   ),
   {
-      flatWhite: ['doppio', 'hot', 'milk'],
-      isValid: [false, false],
-      additionalProp: { thisIsGoodProp: 123 },
-      prop3: true,
-      prop4: true
+    flatWhite: ["doppio", "hot", "milk"],
+    isValid: [false, false],
+    additionalProp: { thisIsGoodProp: 123 },
+    prop3: true,
+    prop4: true,
   }
 );
 
-console.log('Looks good');
+console.log("Looks good");
